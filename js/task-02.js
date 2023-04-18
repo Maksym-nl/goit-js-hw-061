30627da6c98707b630506c06d7b6991d48d05b14
maksymnl <adw0988245131@gmail.com>
2023-04-18 14:19:18 +0300

    DZ6JS

--- js/task-02.js
+++ js/task-02.js
@@ -6,3 +6,13 @@ const ingredients = [
   'Herbs',
   'Condiments',
 ];
+const ulEl = document.getElementById('ingredients');
+const liElements = ingredients.map((ingredient) => {
+let liEl = document.createElement('li')
+  liEl.textContent = ingredient;
+  liEl.classList.add('item');
+  return liEl;
+
+})
+ulEl.append(...liElements);
+console.log(ulEl);
